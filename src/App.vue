aea7cebb8565da55b1b70ba67b5675b81f8f6564
LinShanglei <911930672@qq.com>
2025-07-24 10:50:58 +0800

    完成启动时初始化任务

--- src/App.vue
+++ src/App.vue
@@ -3,7 +3,8 @@ import {onMounted, onUnmounted, ref} from 'vue';
 import {getCurrentWebviewWindow} from '@tauri-apps/api/webviewWindow';
 import {UnlistenFn} from "@tauri-apps/api/event";
 import Database from "@tauri-apps/plugin-sql";
-import {dayjs} from "element-plus";
+import {dayjs, ElMessage} from "element-plus";
+import {invoke} from "@tauri-apps/api/core";
 
 interface TodoItem {
   todo_id: string;
@@ -12,9 +13,69 @@ interface TodoItem {
   task_type: number;
 }
 
+/**
+ * 定时任务对象
+ */
+interface TodoTask {
+  id: string;
+  content: string;
+  remind_time: string;
+  cron_expr: String;
+}
+
 let unlisten: UnlistenFn | null = null;
 
 const db = ref<Database | null>(null);
+
+const cronTaskList = ref<TodoTask[]>([])
+const aperiodicityTask = ref<TodoTask[]>([])
+
+const initCronTask = async () => {
+  try {
+    cronTaskList.value = (await db.value?.select("select id, content, cron_expression as cron_expr from todo_list where is_recurring = 1 and status = 1 and effective = 1")) as TodoTask[] || []
+    if (cronTaskList.value.length > 0) {
+      for (let taskItem of cronTaskList.value) {
+        const task: TodoTask = {
+          id: (BigInt(taskItem.id)).toString(),
+          content: taskItem.content,
+          cron_expr: taskItem.cron_expr,
+          remind_time: ''
+        };
+        await invoke('schedule_cron_task', {task: task});
+      }
+    }
+  } catch (e) {
+    console.log(e)
+    ElMessage({type: 'error', message: `初始化周期性任务失败, ${e}`})
+  }
+}
+
+const initAperiodicityTask = async () => {
+  const currentTime = dayjs().format('YYYY-MM-DD HH:mm:ss')
+  try {
+    aperiodicityTask.value = (await db.value?.select(`select id, content, remind_time
+                                                      from todo_list
+                                                      where is_recurring = 0
+                                                        and status = 0
+                                                        and effective = 1
+                                                        and remind_time >= ${'\'' + currentTime + '\''}`)) as TodoTask[] || []
+    console.log(aperiodicityTask.value.length)
+    if (aperiodicityTask.value.length > 0) {
+      for (let taskItem of aperiodicityTask.value) {
+        const task: TodoTask = {
+          id: (BigInt(taskItem.id)).toString(),
+          content: taskItem.content,
+          cron_expr: taskItem.cron_expr,
+          remind_time: taskItem.remind_time
+        };
+        await invoke('schedule_reminder', {todo: task});
+      }
+    }
+  } catch (e) {
+    console.log(e)
+    ElMessage({type: 'error', message: `初始化非周期性任务失败, ${e}`})
+  }
+}
 
 const updateStatus = async (id: any, type: number) => {
   if (type === 0) {
@@ -33,6 +94,8 @@ onMounted(async () => {
   });
   try {
     db.value = await Database.load('sqlite:test.db');
+    await initCronTask()
+    await initAperiodicityTask()
   } catch (error) {
     console.error('数据库加载失败:', error);
   }
